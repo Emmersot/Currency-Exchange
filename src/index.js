41abd55fb411775d0da15319a98b55d53c9772b2
Tyler Emmerson <TylerEmmerson0216@gmaile.com>
2022-07-12 14:47:51 -0700

    rename for easier formatiting.

--- src/index.js
+++ src/index.js
@@ -5,21 +5,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Exchange from './js/exchange.js';
 
 function clearFields() {
-  $('#setBaseCurrency').val("");
-  $('#setCurrencyToExchange').val("");
-  $('#showExchange').text("");
+  $('#base_code').text("");
+  $('#target_code').text("");
   $('#showError').text("");
 }
 
 function getElements(response) {
   // if (response.conversion_rates["AUD"]) {
-  if (response.conversion_rates[`${currency}`]) {
-    $('${currency}').text(` ${response.conversion_rates['${amount}'].description}`);
+  if (response.conversion_rates[`${target_code}`]) {
+    $('${target_code}').text(` ${response.conversion_rates['${amount}'].description}`);
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
 }
 
-async function makeAPICall(currency) {
-  const response = await 
+async function makeApiCall(currency) {
+  const response = await Exchange.getCurrency(currency);
+  getElements(response);
 }
+
+$(document).ready(function() {
+  $('#exchange').click(function() {
+    let base_code = $('#base_code').val();
+    clearFields();
+    makeApiCall(park);
+  });
+});
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,7 +1,7 @@
 export default class Exchange {
   static async getCurrency(currency) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base_code}/${target_code}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
